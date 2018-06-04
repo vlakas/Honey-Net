7dc1832468cecb6424bb1d7ef0ddd63f2abf123e
Nikola Kipariz Stamov <nikola.k.stamov@protonmail.com>
2018-06-04 13:29:46 +0200

    Create cleaner.sh

---
+++ iktomi/scripts/cleaner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ../logs
+
+echo " " > log25.log 2>/dev/null
+echo " " > log21.log 2>/dev/null
+echo " " > log80.log 2>/dev/null
+echo " " > log8080.log 2>/dev/null
+echo " " > log443.log 2>/dev/null
